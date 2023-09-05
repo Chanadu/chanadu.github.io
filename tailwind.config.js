17fecd8e694b6526fe6d65a7a5a873ecbd5303e4
Chanadu <chandu.peddada@gmail.com>
2023-09-04 20:20:46 -0500

    tailwind and stuff fix i think idk anymroe

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/**/*.{html,js}"],
+  content: ['./dist/**/*.{html,js}', './*.html', './src/js/*.js'],
   theme: {
     extend: {
       height: {
