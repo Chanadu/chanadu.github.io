305b7157e954175923fa1ffdfa68c33535b7eac6
Chanadu <chandu.peddada@gmail.com>
2023-10-15 13:50:51 -0500

    Yo it grid now

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,15 +1,42 @@
 console.log("Hello World")
 
-let hexes : HTMLImageElement[] = [];
+let hexes: HTMLImageElement[] = []
+let hexesHolders: HTMLDivElement[] = []
 let baseHex = document.createElement('img')
 baseHex.id = "hex"
 baseHex.className = "h-52 w-52 absolute transform-gpu"
 baseHex.src = "/imgs/hexagon.svg"
 baseHex.alt = "Hexagon"
+let hexStyle = document.createElement('style')
+hexStyle.id = "hexStyle"
+document.body.append(hexStyle)
+
 
 for (let i = 0; i < 10; i++) {
-    hexes[i] = baseHex.cloneNode()! as HTMLImageElement
-    hexes[i]!.classList.add('left-[500px]')
-    // hexes[i]?.classList.add('left-[200px]')
-    document.getElementById("hexContainer")!.appendChild(hexes[i]!)
+    for (let j = 0; j < 10; j++) {
+        let index = i + j * 10
+
+        hexes[index] = baseHex.cloneNode()! as HTMLImageElement
+        hexes[index]!.id = `hex${i}${j}`
+
+        hexesHolders[index] = document.createElement('div') as HTMLDivElement
+        hexesHolders[index]!.id = `hexHolder${i}${j}`
+
+        hexStyle.append(`#hex${i}${j} {
+                            left: ${(i - .75) * 11.25 + (j % 2 == 0 ? 5.625 : 0)}rem; 
+                            top: ${(j - .5) * 9.7}rem; 
+                            z-index: ${i}${j}
+                        }`)
+
+        document.getElementById("hexContainer")!.appendChild(hexesHolders[index]!)
+        document.getElementById(`hexHolder${i}${j}`)!.appendChild(hexes[index]!)
+
+        hexes[index]!.onmouseenter = () => {
+
+        }
+
+        hexes[index]!.onmouseleave = () => {
+
+        }
+    }
 }
