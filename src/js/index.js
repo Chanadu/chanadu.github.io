69b7dabc3b8fba222f1a30369639c75eea72d5e4
Chanadu <chandu.peddada@gmail.com>
2023-11-13 21:20:09 -0600

    Kinda doesnt work but we move

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,8 @@
 "use strict";
-console.log("Hello World");
-let hexes = [];
-let hexesHolders = [];
+// console.log("Hello World")
 let baseHex = document.createElement('img');
+let hexes = [...Array(10)].map(() => Array(10));
+let hexesHolders = [...Array(10)].map(() => Array(10));
 baseHex.id = "hex";
 baseHex.className = "absolute transform-gpu"; //h-52 w-52
 baseHex.src = "/imgs/hexagon.svg";
@@ -13,23 +13,38 @@ baseHex.alt = "Hexagon";
 let hexStyle = document.createElement('style');
 hexStyle.id = "hexStyle";
 document.body.append(hexStyle);
+let mouseOverHexes = [];
+let currentMouseOverHex = [];
+hexStyle.append(`
+.opacity0 {
+    opacity: 0;
+}
+`);
 for (let i = 0; i < 10; i++) {
     for (let j = 0; j < 10; j++) {
-        let index = i + j * 100;
-        hexes[index] = baseHex.cloneNode();
-        hexes[index].id = `hex${index}`;
-        hexesHolders[index] = document.createElement('div');
-        hexesHolders[index].id = `hexHolder${index}`;
-        hexStyle.append(`#hex${index} {
+        hexes[i][j] = baseHex.cloneNode();
+        hexes[i][j].id = `hex${i}_${j}`;
+        hexesHolders[i][j] = document.createElement('div');
+        hexesHolders[i][j].id = `hexHolder${i}_${j}`;
+        hexStyle.append(`#hex${i}_${j} {
                             left: ${(i - (.96 * factor)) * (factor * 11.25) + (j % 2 == 0 ? (5.625 * factor) : 0)}rem; 
                             top: ${(j - (factor * .23)) * (factor * 9.7)}rem; 
-                            z-index: ${index}
+                            z-index: ${i + j * 100}
                         }`);
-        document.getElementById("hexContainer").appendChild(hexesHolders[index]);
-        document.getElementById(`hexHolder${index}`).appendChild(hexes[index]);
-        hexes[index].onmouseenter = () => {
+        document.getElementById("hexContainer").appendChild(hexesHolders[i][j]);
+        document.getElementById(`hexHolder${i}_${j}`).appendChild(hexes[i][j]);
+        hexes[i][j].onmouseenter = () => {
+            mouseOverHexes.push([i, j]);
+            hexesHolders[i][j].classList.add("opacity0");
         };
-        hexes[index].onmouseleave = () => {
+        hexes[i][j].onmouseleave = () => {
+            for (let k = 0; k < mouseOverHexes.length; k++) {
+                if (mouseOverHexes[k][0] == i && mouseOverHexes[k][1] == j) {
+                    mouseOverHexes.splice(k, 1);
+                    break;
+                }
+            }
+            hexesHolders[i][j].classList.remove("opacity0");
         };
     }
 }
--- src/js/index.ts
+++ src/js/index.ts
@@ -1,45 +1,59 @@
-console.log("Hello World")
+// console.log("Hello World")
 
-let hexes: HTMLImageElement[] = []
-let hexesHolders: HTMLDivElement[] = []
-let baseHex = document.createElement('img')
+let baseHex : HTMLImageElement = document.createElement('img')
+let hexes : HTMLImageElement[][] = [...Array(10)].map(() => Array(10));
+let hexesHolders : HTMLDivElement[][] = [...Array(10)].map(() => Array(10));
+
 baseHex.id = "hex"
 baseHex.className = "absolute transform-gpu" //h-52 w-52
 baseHex.src = "/imgs/hexagon.svg"
-let factor = Math.max((window.screen.availWidth / 1920), (window.screen.availHeight / 1920)) * 1.5
+let factor : number = Math.max((window.screen.availWidth / 1920), (window.screen.availHeight / 1920)) * 1.5
 baseHex.width = 180.620956 * factor
 baseHex.height = 208 * factor
 baseHex.alt = "Hexagon"
-let hexStyle = document.createElement('style')
+let hexStyle : HTMLStyleElement = document.createElement('style')
 hexStyle.id = "hexStyle"
 document.body.append(hexStyle)
 
+let mouseOverHexes : number[][] = [];
+let currentMouseOverHex : number[] = [];
+hexStyle.append(`
+.opacity0 {
+    opacity: 0;
+}
+`)
+for (let i : number = 0; i < 10; i++) {
+    for (let j : number = 0; j < 10; j++) {
 
-for (let i = 0; i < 10; i++) {
-    for (let j = 0; j < 10; j++) {
-        let index = i + j * 100
+        hexes[i]![j] = baseHex.cloneNode()! as HTMLImageElement
+        hexes[i]![j]!.id = `hex${i}_${j}`
 
-        hexes[index] = baseHex.cloneNode()! as HTMLImageElement
-        hexes[index]!.id = `hex${index}`
+        hexesHolders[i]![j] = document.createElement('div') as HTMLDivElement
+        hexesHolders[i]![j]!.id = `hexHolder${i}_${j}`
 
-        hexesHolders[index] = document.createElement('div') as HTMLDivElement
-        hexesHolders[index]!.id = `hexHolder${index}`
-
-        hexStyle.append(`#hex${index} {
+        hexStyle.append(`#hex${i}_${j} {
                             left: ${(i - (.96 * factor)) * (factor * 11.25) + (j % 2 == 0 ? (5.625 * factor) : 0)}rem; 
                             top: ${(j - (factor * .23)) * (factor * 9.7)}rem; 
-                            z-index: ${index}
+                            z-index: ${i + j*100}
                         }`)
 
-        document.getElementById("hexContainer")!.appendChild(hexesHolders[index]!)
-        document.getElementById(`hexHolder${index}`)!.appendChild(hexes[index]!)
+        document.getElementById("hexContainer")!.appendChild(hexesHolders[i]![j]!)
+        document.getElementById(`hexHolder${i}_${j}`)!.appendChild(hexes[i]![j]!)
 
-        hexes[index]!.onmouseenter = () => {
-            
+        hexes[i]![j]!.onmouseenter = () => {
+            mouseOverHexes.push([i, j]);
+            hexesHolders[i]![j]!.classList.add("opacity0");
         }
 
-        hexes[index]!.onmouseleave = () => {
+        hexes[i]![j]!.onmouseleave = () => {
+            for (let k : number = 0; k < mouseOverHexes.length; k++) {
+                if (mouseOverHexes[k]![0] == i && mouseOverHexes[k]![1] == j) {
+                    mouseOverHexes.splice(k, 1);
+                    break;
+                }
+            }
 
+            hexesHolders[i]![j]!.classList.remove("opacity0");
         }
     }
 }
