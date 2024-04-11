218d5cb4c1239df7f4d2fd8581fc7c997ec39ecd
Chanadu <chandu.peddada@gmail.com>
2024-04-11 18:09:07 -0500

    tbh idk anymore

--- src/components/HomePage/HorizontalBar.vue
+++
@@ -1,7 +0,0 @@
-<template>
-	<div class="w-80 h-1 bg-slate-100"></div>
-</template>
-
-<script setup lang="ts"></script>
-
-<style scoped></style>
--- src/components/ZoomedOutPage/SmallPageTemplate.vue
+++
@@ -1,9 +0,0 @@
-<template>
-	<div class="min-w-[16rem] min-h-[9rem]">
-		<slot></slot>
-	</div>
-</template>
-
-<script setup lang="ts"></script>
-
-<style scoped></style>
